eb8169fef2e63db300da9e5e90bf9e1dbcb84701
Harsh <harsh.anand20999@gmail.com>
2022-11-12 20:51:49 +0530

    Updates

---
+++ precache-manifest.49ce86dd10c8ac2a4313a3dd4e00ddfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149dc48288e824ca57112c7d4377c3dc",
+    "url": "/memories-app/index.html"
+  },
+  {
+    "revision": "78e58eca374f14697206",
+    "url": "/memories-app/static/css/main.25336470.chunk.css"
+  },
+  {
+    "revision": "a7dad7bab24b5d1d573c",
+    "url": "/memories-app/static/js/2.63646601.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/memories-app/static/js/2.63646601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e58eca374f14697206",
+    "url": "/memories-app/static/js/main.689a0582.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/memories-app/static/js/main.689a0582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a1a0ea9ef8d4b317ff",
+    "url": "/memories-app/static/js/runtime-main.e302f67b.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/memories-app/static/media/memories.5c0c63fb.png"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/memories-app/static/media/memoriesLogo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/memories-app/static/media/memoriesText.6c7b5ec0.png"
+  }
+]);
